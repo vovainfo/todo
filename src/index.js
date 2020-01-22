67fcc0c7954cc3930fc99039d1eadd798a2b724c
Vladimir Gumennikov <vova@vova.info>
2020-01-23 00:44:37 +1000

    23.01

--- src/index.js
+++ src/index.js
@@ -1,25 +1,7 @@
-import React from "react";
-import ReactDom from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import AppHeader from "./components/app-header";
-import SearchPanel from "./components/search-panel";
-import TodoList from "./components/todo-list";
+import App from './components/app';
 
-
-const App = ( ) => {
-    const todoData = [
-        {label: 'Drink', important: false, id: 1 },
-        {label: 'App', important: true, id: 2 },
-        {label: 'Жри!', important: false, id: 3 }
-    ];
-    return (
-        <div>
-            <AppHeader/>
-            <SearchPanel/>
-            <TodoList todos={todoData}/>
-        </div>
-    );
-};
-
-ReactDom.render(<App/>, document.getElementById('root'));
-
+ReactDOM.render(<App />,
+  document.getElementById('root'));
