6e83976abac2d5d389e9020704e038af6a2cb82b
Vladimir Gumennikov <vova@vova.info>
2020-01-22 10:54:13 +1000

    22.01

---
+++ src/components/todo-list.js
@@ -0,0 +1,13 @@
+import React from "react";
+import TodoListItem from "./todo-list-item";
+
+const TodoList = ( ) => {
+    return (
+        <ul>
+            <li><TodoListItem label="dddddd"/></li>
+            <li><TodoListItem label="dddddd12" important/></li>
+        </ul>
+    );
+};
+
+export default TodoList
---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDom from 'react-dom';
+
+import AppHeader from "./components/app-header";
+import SearchPanel from "./components/search-panel";
+import TodoList from "./components/todo-list";
+
+
+const App = ( ) => {
+    return (
+        <div>
+            <AppHeader/>
+            <SearchPanel/>
+            <TodoList/>
+        </div>
+    );
+};
+
+ReactDom.render(<App/>, document.getElementById('root'));
+
