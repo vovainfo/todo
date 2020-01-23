648f712cf05ad1284ac91522bea1a14d4d59f5cb
Vladimir Gumennikov <>
2020-01-23 16:52:56 +1000

    23.01 на работе

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -1,33 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './todo-list-item.css';
 
-const TodoListItem = ({ label, important = false }) => {
+export default class TodoListItem extends Component {
 
-  const style = {
-    color: important ? 'steelblue' : 'black',
-    fontWeight: important ? 'bold' : 'normal'
-  };
+    onLabelClick = () => {
+        console.log(`Done ${this.props.label}`);
+    };
 
-  return (
-    <span className="todo-list-item">
-      <span
-        className="todo-list-item-label"
-        style={style}>
-        {label}
-      </span>
+    render(){
 
-      <button type="button"
-              className="btn btn-outline-success btn-sm float-right">
-        <i className="fa fa-exclamation" />
-      </button>
+        const { label, important = false } = this.props;
+        const style = {
+            color: important ? 'steelblue' : 'black',
+            fontWeight: important ? 'bold' : 'normal'
+        };
 
-      <button type="button"
-              className="btn btn-outline-danger btn-sm float-right">
-        <i className="fa fa-trash-o" />
-      </button>
-    </span>
-  );
-};
+        return (
+            <span className="todo-list-item">
+              <span
+                  className="todo-list-item-label"
+                  style={style}
+                  onClick={ this.onLabelClick }  >
+                {label}
+              </span>
 
-export default TodoListItem;
+              <button type="button"
+                      className="btn btn-outline-success btn-sm float-right">
+                <i className="fa fa-exclamation" />
+              </button>
+
+              <button type="button"
+                      className="btn btn-outline-danger btn-sm float-right">
+                <i className="fa fa-trash-o" />
+              </button>
+            </span>
+        );
+    }
+}
+
+
