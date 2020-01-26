1f4aa2b586b39a648e60955f0b71ef785c3cb09f
Vladimir Gumennikov <vova@vova.info>
2020-01-26 23:53:09 +1000

    26.01

---
+++ src/components/item-add-form/index.js
@@ -0,0 +1,4 @@
+import ItemAddForm from './item-add-form';
+
+import './item-add-form.css'
+export default ItemAddForm;
---
+++ src/components/item-add-form/item-add-form.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+
+export default class ItemAddForm extends Component{
+
+    state = {
+        label: ''
+    };
+    onLabelChange = (e) => {
+        this.setState({
+            label: e.target.value.toUpperCase()
+        });
+    };
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        this.props.onItemAdded(this.state.label);
+        this.setState({label: ''});
+    };
+
+    render() {
+        return (<form className="item-add-form d-flex"
+                      onSubmit={this.onSubmit}>
+                    <input type="text"
+                           className="form-control"
+                           onChange={this.onLabelChange}
+                           placeholder="Что надо?"
+                           value={this.state.label}
+                    />
+                    <button className="btn btn-outline-secondary">
+                        Add item
+                    </button>
+                </form>
+        )
+
+    }
+}
